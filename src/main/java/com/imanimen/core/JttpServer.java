4788c1eb48be181c8e525b043bced9bf29ab3b7c
Iman Imen <imanimen81@gmail.com>
2026-07-05 14:17:14 +0330

    chore(build): fixed dependencies + adding configuration in the Jttp

--- src/main/java/com/imanimen/core/JttpServer.java
+++ src/main/java/com/imanimen/core/JttpServer.java
@@ -1,10 +1,18 @@
 package com.imanimen.core;
 
+import com.imanimen.core.config.Configuration;
+import com.imanimen.core.config.ConfigurationManager;
+
 /**
  * Driver Class for the Jttp Server
  */
 public class JttpServer {
     public static void main(String[] args) {
         System.out.println("Starting JttpServer");
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/jttp.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+        System.out.println("Current configuration: " + conf);
+        System.out.println("Using Port: " + conf.getPort());
+        System.out.println("Using WebRoot: " + conf.getWebroot());
     }
 }
